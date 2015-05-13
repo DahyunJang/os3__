a463eaca4b1cc8e7540704bd170a94e2cf87c682
DahyunJang <jangdh93@gmail.com>
2015-05-14 05:40:37 +0900

    2/3

--- vm/page.c
+++ vm/page.c
@@ -28,6 +28,11 @@ bool swap_out (struct frame_entry *fe)
 
   if (fe == NULL || sd.swap_block == NULL || sd.swap_map == NULL)
     PANIC ("swap_out : NULL failed");
+  if(fe->s_ent == NULL)
+    PANIC ("swap_out : s_ent is NULL");
+  if (!fe->s_ent->type_swap)
+    PANIC ("swap_out : this is not type_swap");
+
   lock_acquire(&sd.lock);
 
   sect_start = bitmap_scan_and_flip (sd.swap_map, 0, 1, false); 
@@ -51,6 +56,14 @@ bool swap_in (struct frame_entry *fe)
 
   if (fe == NULL || sd.swap_block == NULL || sd.swap_map == NULL)
     PANIC ("swap_in : NULL failed");
+
+  if(fe->s_ent == NULL)
+    PANIC ("swap_in : s_ent is NULL");
+
+  if (!fe->s_ent->type_swap)
+    PANIC ("swap_in : this is not type_swap");
+
+
   lock_acquire(&sd.lock);
 
   sect_start = fe->s_ent->swap_idx;
@@ -67,25 +80,208 @@ bool swap_in (struct frame_entry *fe)
 }  
 
 
-bool frame_init ()
+bool fe_init ()
 {
   lock_init(&ft.lock);
   list_init(&ft.frame_list);
 }
 
+struct frame_entry* fe_alloc (struct sup_entry *se)
+{ 
+  struct frame_entry *fe;
 
-bool frame_evict () 
+  if (se == NULL) 
+    PANIC ("frame_alloc: se is NULL");
+  
+  lock_acquire (&ft.lock);
+
+  fe = malloc (sizeof(struct frame_entry));
+ 
+  fe->frame = palloc_get_page (PAL_USER);
+  if (fe->frame == NULL)
+    {
+      fe_evict();
+      fe->frame = palloc_get_page (PAL_USER);
+      ASSERT (fe->frame);
+    }
+  if (list_insert (&ft.frame_list, fe->elem) == NULL)
+    PANIC ("frame_alloc : list insert failed");
+ 
+  fe->s_ent = se;
+  se->fe = fe;
+  se->thread = thread_current ();
+
+  lock_release (&ft.lock);
+  return fe;
+}
+
+bool fe_remove (struct frame_entry *fe)
 {
+  struct list_elem *iter;
+  struct frame_entry *fe_tmp;
+  bool frame_removed = false;
+  lock_acquire(&ft.lock);
+  
+  for (iter = list_begin(&ft); iter != list_end(&ft);
+       iter = list_next(iter)){
+    
+    fe_tmp = list_entry(iter, struct frame_entry, elem);
+    if (fe == fe_tmp)
+      {	
+	list_remove(iter);
+	palloc_free_page(fe->frame);
+	free(fe);
+	frame_removed = true;
+	break;
+      }
+  }
+  lock_release (&ft.lock);
 
-
+  return frame_removed; 
 }
 
 
-bool frame_destroy (struct frame_entry *fe) {
-  if (fe == NULL) return;
+bool fe_evict ()
+{
+  struct list_elem *iter;
+  struct frame_entry *fe;
+  bool ret = false;
+  bool is_accessed = false;
 
-  //remove frame from list
-  //free allocated frame
+  //find fe to evict
+  // alliased page which is not generated from fe_alloc???
+  for (iter = list_begin (&ft.frame_list); ;  ) 
+    {
+      LIST_ENTRY () 
 
+      if (pagedir_is_accessed (fe->se->t->pagedir, fe->se->uva))
+	{
+	  pagedir_set_accessed (t->pagedir, fe->se->uva, false);
+	}
+
+      else break;
+    }
+  
+  if (fe->s_ent->type_swap) 
+    {
+      swap_out (fe); 
+      ret = fe_remove (fe);     	
+    }
+
+  else 
+    { 
+      //project 3-2, other types like file
+      //if page is dirty write to file
+    }
+  
+  return ret;
+}
+
+
+
+
+static unsigned page_hash_func (const struct hash_elem *e, void *aux UNUSED)
+{
+  struct sup_entry *se = hash_entry(e, struct sup_entry, elem);
+  return hash_int((int) se->uva);
+}
+
+static bool page_less_func (const struct hash_elem *a,
+			    const struct hash_elem *b,
+			    void *aux UNUSED)
+{
+  struct sup_entry *sa = hash_entry(a, struct sup_entry, elem);
+  struct sup_entry *sb = hash_entry(b, struct sup_entry, elem);
+ 
+  return (sa->uva < sb->uva);
+}
+
+static void page_destroy_func (struct hash_elem *e, void *aux UNUSED)
+{  
+  struct sup_entry *se_h = hash_entry(e, struct sup_entry, elem);
+  
+  if (se_h->fe != NULL)
+    {
+      if (se_h->ali_prev == NULL && se_h->ali_next == NULL) 
+	fe_remove (se_h->fe);
+
+      else { //when page is aliased, do not remove frame
+	//aliased pages are chained in doubly linked list
+	if (se_h->ali_prev == NULL)//head
+	  se_h->fe->se = se_h->ali_next;
+
+	else{
+	  se_h->ali_prev->ali_next = se_h->ali_next;
+
+	  if (se_h->ali_next != NULL)
+	    se_h->ali_next->ali_prev = se_h->ali_prev;	  
+	}
+      }
+
+      pagedir_clear_page(thread_current()->pagedir, se_h->uva);
+    }
+  free(se_h);
+}
+
+void sup_init ()
+{
+  init_lock (&thread_current()->spt.lock);
+  hash_init (&thread_current()->spt.sup_hash, page_hash_func, page_less_func, NULL);
+}
+
+void pt_destroy ()
+{
+  hash_destroy (&thread_current()->spt.sup_hash, page_destroy_func);
+}
+
+struct sup_entry* get_se (void *uva)
+{
+  struct sup_entry se;
+  se.uva = pg_round_down(uva);
+
+  struct hash_elem *e = hash_find(&thread_current()->spt, &se.elem);
+  if (e == NULL)
+    {
+      return NULL;
+    }
+  return hash_entry (e, struct sup_entry, elem);
+}
+
+
+
+
+void load_swap (struct sup_entry *se)
+{
+  struct sup_entry * se_tmp;
+  se_tmp = se;
+
+  //only ali header has swap_idx
+  while (se_tmp->ali_prev == NULL) 
+    se_tmp = se_tmp->ali_prev;
+
+  struct frame_enry *fe = fe_alloc(se_tmp);
+  swap_in (fe);
+}
+
+bool stack_grow (void *uva) 
+{
+  if (PHYS_BASE - pg_round_down(uva) > MAX_STACK_SIZE)
+      return false;
+
+  struct sup_entry *se = malloc(sizeof(struct sup_entry));
+  if (se == NULL)
+      return false;
+
+  se->uva = pg_round_down(uva);
+  se->type_swap = true;
+  
+  fe_alloc (se);
+
+
+  //install page here
+  //install se-fe 
+
+  hash_insert(&thread_current()->spt, &se->elem);
+  return true;
 }
 
--- vm/page.h
+++ vm/page.h
@@ -15,37 +15,7 @@
 #define SECTORS_PER_PAGE (PGSIZE/BLOCK_SECTOR_SIZE) 
 
 
-
-
-struct sup_page_table {
-  struct lock lock;
-  struct hash sup_hash;
-} spt;
-
-struct sup_entry {
-  struct hash_elem elem;
-  block_sector_t swap_idx;
-  //vaddr
-  // validity?
-  
-  //free rsc when process terminated
-  //exception error code? or page fault..
-} 
-
-struct frame_table {
-  struct lock lock;
-  struct list frame_list;
-} ft;
-
-struct frame_entry {
-  struct list_elem elem;
-
-  struct threads thread;
-  struct sup_entry *s_ent;
-
-  void *frame;
-}
-
+/*------------------- SWAP --------------------*/
 struct swap_disk {
   struct lock lock;
   struct bitmap *swap_map;
@@ -55,17 +25,56 @@ struct swap_disk {
 
 } sd;
 
-void sup_init ();
-get_disk_page();
-stack_growth (); //interrupt frame
-
-
-bool frame_init ();
-bool frame_evict ();
-bool frame_create ();
-bool frame_destroy (struct frame_entry *);
 
 bool swap_init ();
 bool swap_in (struct frame_entry *);
 bool swap_out (struct frame_entry *);
+
+/*----------------- FRAME TABLE -----------------*/
+struct frame_table {
+  struct lock lock;
+  struct list frame_list;
+} ft;
+
+struct frame_entry {
+  struct list_elem elem;
+
+  
+  struct sup_entry *s_ent;
+
+  void *frame;
+}
+
+
+
+/*---------------SUP PAGE TABLE-----------------*/
+
+struct sup_table {
+  struct lock lock;
+  struct hash sup_hash;
+}; 
+//in threads structure, 
+//plz declare "struct sup_table spt";
+
+struct sup_entry {
+  void *uva;
+  struct threads *thread;
+  struct hash_elem elem;
+  bool type_swap;
+
+
+
+  struct frame_entry *fe;
+  /* aliasing -> page list : modify page installation  */  
+  struct sup_entry *ali_prev;
+  struct sup_entry *ali_next;
+  //only head(ali_prev == NULL) alias have swap_idx.
+  block_sector_t swap_idx;
+  
+  //free rsc when process terminated
+  //exception error code? or page fault..
+} 
+
+
+bool sup_init ();
 
